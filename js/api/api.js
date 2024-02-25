3054b8de2c9126a65e3693a17bb91fc1e5880da8
Houssam Younes <houssamyounes@Houssams-MacBook-Pro.local>
2024-02-25 21:14:27 +0200

    return original api endpoint

--- js/api/api.js
+++ js/api/api.js
@@ -1,18 +1,17 @@
 import { toggleLoading } from "../navigation.js";
 
 export async function fetchInventory() {
-    try {
-      toggleLoading(true);
-      console.log('fetching...');
-      const response = await fetch("/api/inventoryy");
-      toggleLoading(false);
-      if (!response.ok){
-        throw new Error("Failed - Could not fetch inventory");
-      }
-      return response.json();
-    } catch (error) {
-      console.error("Error fetching inventory:", error);
-      return null;
+  try {
+    toggleLoading(true);
+    console.log('fetching...');
+    const response = await fetch("/api/inventory");
+    toggleLoading(false);
+    if (!response.ok) {
+      throw new Error("Failed - Could not fetch inventory");
     }
+    return response.json();
+  } catch (error) {
+    console.error("Error fetching inventory:", error);
+    return null;
   }
-  
+}
