efeee61ec53d9a09ef067874fa46c043fe74cf66
Houssam Younes <houssamyounes@Houssams-MacBook-Pro.local>
2024-03-09 00:31:53 +0200

    hide averages

--- js/file-constants.js
+++ js/file-constants.js
@@ -86,6 +86,6 @@ export const columnsForUpdate = [
 
 
 export const featureFlags = {
-  showCostComparisons: true,
-  showPercentages: true
+  showCostComparisons: false,
+  showPercentages: false
 };
