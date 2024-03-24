f6f1c9c309c9c140c26d05c71b7045eee34a6a8b
Houssam Younes <houssamyounes@Houssams-MacBook-Pro.local>
2024-03-24 16:26:38 +0200

    log

--- js/api/api.js
+++ js/api/api.js
@@ -3,12 +3,13 @@ import { toggleLoading } from "../navigation.js";
 export async function fetchInventory() {
   try {
     toggleLoading(true);
-    console.log('fetching...');
+    console.log('fetching from db...');
     const response = await fetch("/api/inventory");
     toggleLoading(false);
     if (!response.ok) {
       throw new Error("Failed - Could not fetch inventory");
     }
+    console.log('done');
     return response.json();
   } catch (error) {
     console.error("Error fetching inventory:", error);
